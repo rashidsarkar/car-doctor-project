b63b10fb1f185a60f082667d9b270877581ee134
rashidsarkar <rashidrock558@gmail.com>
2023-10-28 17:09:54 +0600

    withCredentials setUP

--- src/AxiosAPI/axiosInstance.js
+++ src/AxiosAPI/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000", // Your API base URL
+  withCredentials: true,
   timeout: 5000, // Adjust the timeout as needed
 });
 
